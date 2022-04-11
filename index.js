571f8a7dc47e9f0f9acac0ef98b2f916e0f6fa70
Nicholas Palomba <nicholaspalomba16@gmail.com>
2022-04-11 14:28:36 -0400

    finished

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    return this.length++ 
+  }
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let sortedArr = this.items.sort(function(a, b){
+      return b - a; 
+    })
+    return sortedArr[0]; 
+   }
 
-  get(pos) {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let sortedArr = this.items.sort(function(a, b){
+      return a - b; 
+    })
+    return sortedArr[0]; 
+   }
 
-  max() {}
+  sum() { 
+    if (this.length === 0) {
+      return 0; 
+    }
+    let sumArr = this.items.reduce(function(total, num){
+      return total + num;
+    })
+    return sumArr; 
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() { 
+    if (this.length === 0) {
+      throw new Error('EmptySortedList'); 
+    }
+    let sumArr = this.items.reduce(function(total, num){
+      return total + num;
+    })
+    return sumArr/this.length;
+  }
 }
+
+
+
 
 module.exports = SortedList;
